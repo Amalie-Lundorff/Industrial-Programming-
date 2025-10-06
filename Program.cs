a912151fa93d04ee40027cc003bf81544db37e85
Amalie-Lundorff <s235664@dtu.dk>
2025-10-06 19:37:44 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,58 @@
-using System;             // <-- nødvendig for [STAThread]
-using Avalonia;
+﻿using System;
 
-namespace AvaloniaApplication2_week5_RPSSL_GUI;
+// Jeg bruger enum til tabellen og regner forskellen (p2 - p1), og laver en switch for at finde resultatet
 
-internal static class Program
+// Enum: giver navne til tallene som ses i tabellen fra opgaven
+enum Shape { Rock = 0, Paper = 1, Scissors = 2, Lizard = 3, Spock = 4 }
+
+class Program
 {
-    [STAThread]
-    public static void Main(string[] args)
-        => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    static void Main()
+    {
+        // Tester alle kombinationerne 
+        foreach (Shape p1 in Enum.GetValues<Shape>())
+        foreach (Shape p2 in Enum.GetValues<Shape>())
+        {
+            int diff = (int)p2 - (int)p1;    //Forskellene som i tabellen
+            string result;
 
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>().UsePlatformDetect().LogToTrace();
+            // Afgør udfaldet
+            switch (diff)
+            {
+                case 0:
+                    result = "tie";
+                    break;
+
+                // p1 vinder
+                case -4:
+                case -2:
+                case 1:
+                case 3:
+                    result = "p1 wins";
+                    break;
+
+                // p1 taber
+                case -3:
+                case -1:
+                case 2:
+                case 4:
+                    result = "p1 loses";
+                    break;
+
+                //Hvis diff ikke passer til nogen af de andre, sættes resultat til "unexpected"
+                default:
+                    result = "unexpected";
+                    break;
+            }
+
+            //Viser spiller 1's valg og spiller 2's valg. Til slut vises resultatet på skærmen
+            Console.WriteLine($"If p1: {p1} and p2: {p2} => {result}");
+        }
+    }
 }
+
+//Noter til funktioner i koderne 
+    //enum Shape giver navne til tallene 0-4
+    //p2-p1 giver forskellen, som matcher i tabellen fra opgaven
+    //switch (diff) afgør resultatet (tie, win eller lose)
+    //foreach tester alle de mulige kombinationer 
