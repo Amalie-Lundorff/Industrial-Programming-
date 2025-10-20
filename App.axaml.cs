b603eb2458e1de316a0b50d1e9172864083dccd4
Amalie-Lundorff <s235664@dtu.dk>
2025-10-20 18:27:13 +0200

    Add files via upload

--- App.axaml.cs
+++ App.axaml.cs
@@ -2,16 +2,22 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace AvaloniaApplication2_week5_RPSSL_GUI;
+namespace InventorySystems_Week6;
 
 public partial class App : Application
 {
-    public override void Initialize() => AvaloniaXamlLoader.Load(this);
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
 
     public override void OnFrameworkInitializationCompleted()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desk)
-            desk.MainWindow = new GameWindow();  // <-- bruger code-only vinduet
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+        }
+
         base.OnFrameworkInitializationCompleted();
     }
 }
