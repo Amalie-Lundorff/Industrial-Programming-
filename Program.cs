b1ad9e16c2452814ed2de75cdc22809c4d054d4b
Amalie-Lundorff <s235664@dtu.dk>
2025-10-20 18:31:06 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,58 +1,21 @@
-﻿using System;
+﻿using Avalonia;
+using System;
 
-// Jeg bruger enum til tabellen og regner forskellen (p2 - p1), og laver en switch for at finde resultatet
-
-// Enum: giver navne til tallene som ses i tabellen fra opgaven
-enum Shape { Rock = 0, Paper = 1, Scissors = 2, Lizard = 3, Spock = 4 }
+namespace InventorySystems_Week6;
 
 class Program
 {
-    static void Main()
-    {
-        // Tester alle kombinationerne 
-        foreach (Shape p1 in Enum.GetValues<Shape>())
-        foreach (Shape p2 in Enum.GetValues<Shape>())
-        {
-            int diff = (int)p2 - (int)p1;    //Forskellene som i tabellen
-            string result;
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
-            // Afgør udfaldet
-            switch (diff)
-            {
-                case 0:
-                    result = "tie";
-                    break;
-
-                // p1 vinder
-                case -4:
-                case -2:
-                case 1:
-                case 3:
-                    result = "p1 wins";
-                    break;
-
-                // p1 taber
-                case -3:
-                case -1:
-                case 2:
-                case 4:
-                    result = "p1 loses";
-                    break;
-
-                //Hvis diff ikke passer til nogen af de andre, sættes resultat til "unexpected"
-                default:
-                    result = "unexpected";
-                    break;
-            }
-
-            //Viser spiller 1's valg og spiller 2's valg. Til slut vises resultatet på skærmen
-            Console.WriteLine($"If p1: {p1} and p2: {p2} => {result}");
-        }
-    }
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 }
-
-//Noter til funktioner i koderne 
-    //enum Shape giver navne til tallene 0-4
-    //p2-p1 giver forskellen, som matcher i tabellen fra opgaven
-    //switch (diff) afgør resultatet (tie, win eller lose)
-    //foreach tester alle de mulige kombinationer 
